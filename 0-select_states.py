9c56c561c936c5b5935a9b4b3e4e445a2f80c9e1
KellsCodes <nworjiifeanyi@gmail.com>
2023-03-18 23:45:03 +0000

    fixing pycodestyle error on indentation

--- 0-select_states.py
+++ 0-select_states.py
@@ -12,7 +12,8 @@ _args = sys.argv
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=_args[1],
-            passwd=_args[2], db=_args[3], charset='utf8', port=3306)
+                         passwd=_args[2], db=_args[3],
+                         charset='utf8', port=3306)
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     query_rows = cur.fetchall()
@@ -20,4 +21,3 @@ if __name__ == "__main__":
         print(row)
     cur.close()
     db.close()
-
